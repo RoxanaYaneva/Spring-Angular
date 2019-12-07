5ae5ece34796cafd5015364bd4228116bda52e0b
Roxana Yaneva <roxy.1569@gmail.com>
2019-12-07 11:33:55 +0200

    Initializing project

---
+++ RoxStar Games Project/server/roxstar-games-api/src/main/java/mvc/spring/roxstargamesapi/RoxStarGamesApplication.java
@@ -0,0 +1,13 @@
+package mvc.spring.roxstargamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoxStarGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoxStarGamesApplication.class, args);
+	}
+
+}
